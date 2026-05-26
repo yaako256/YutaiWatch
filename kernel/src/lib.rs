8a286185fbc8211a23531d95a87eff18997c8858
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-05-27 04:08:28 +0900

    feat: #43 kernelのrun_monitorを仮で作る

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,10 +1,11 @@
 use infra_config::AppConfig;
-use shared::ScrapedItem;
-use shared::errors::AppResult;
+use shared::constants::file::NOTIFIED_KEYS_LIMIT;
+use shared::errors::{AppError, AppResult};
+use shared::{ScrapedItem, ScraperOutput, State};
 
 // 外部ライブラリ
 // ログ出力
-use tracing::info;
+use tracing::error;
 
 /// デバッグ用関数
 pub fn debug() {
@@ -18,62 +19,154 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
   monitor::debug();
   discord::debug();
 
-  // スクレイピング部分実行のテスト
-  let output = match infra::scraper::run_scraper(&config.scraper) {
-    Ok(o) => o,
+  // ----------------------
+  // stateデータ取得(なければ初回フローの実行)
+  // ----------------------
+  // stateをロード
+  let state: AppResult<Option<State>> = infra::storage::load_state(&config.data.dir_path.as_path());
+  // 取得に成功したか
+  // 取得に失敗したら初回フロー
+  let state: Option<State> = match state {
+    Ok(s) => s,
     Err(e) => {
-      info!("{:#?}", e);
-      return Ok(());
+      // デバッグログ
+      error!("Stateの取得に失敗。initフローの実行をしましす:{}", e);
+
+      // 終了処理(未実装)
+      // finish()
+
+      // Err返し終了
+      return Err(AppError::Process(format!(
+        "Stateの取得に失敗。initフローの実行をしました: {}",
+        e
+      )));
     }
   };
 
-  // stateデータ取得のテスト
-  let state = infra::storage::load_state(&config.data.dir_path.as_path());
-  info!("{:#?}", state);
-
-  let state = state?;
-
-  let state = match state {
+  // stateに中身があったか
+  // なかったら初回フロー
+  let mut state: State = match state {
     Some(s) => s,
     None => {
-      println!("値なし");
-      return Ok(());
+      // デバッグログ
+      //error!("Stateに値なし。initフローの実行をしましす:{}", e);
+
+      // 初回フローの実行(未実装)
+      // init()
+
+      // 終了処理(未実装)
+      // finish()
+
+      // Err返し終了
+      return Err(AppError::Process(
+        "Stateに中身がなかったためinitフローを実行しました".to_string(),
+      ));
     }
   };
 
-  // 差分検出のデバッグ
+  // ----------------------
+  // 監視対象サイトをスクレイピング
+  // pythonを実行
+  // ----------------------
+  // スクレイピング部分実行のテスト
+  let output: ScraperOutput = match infra::scraper::run_scraper(&config.scraper) {
+    Ok(o) => o,
+    Err(e) => {
+      // 終了処理(未実装)
+      // finish()
+
+      // Err返し終了
+      return Err(AppError::Process(format!("{}", e)));
+    }
+  };
+
+  // ----------------------
+  // 更新差分判定
+  // 更新なし → 終了処理
+  // 更新あり → 処理実行
+  // ----------------------
   let new_data: Vec<(String, ScrapedItem)> = monitor::detect_new_item(&output, &state)?;
 
-  // ScrapedItemだけ取り出す
-  let items: Vec<ScrapedItem> = new_data.into_iter().map(|(_, item)| item).collect();
+  // 空だったらdetect_history更新だけして実行終了
+  if new_data.is_empty() {
+    // detect_historyの更新
+    infra::storage::append_detect_history(
+      config.data.dir_path.as_path(),
+      &shared::DetectHistory {
+        detected_at: output.fetched_at,
+        updated: false,
+      },
+    )?;
 
+    // 終了処理(未実装)
+    // finish()
+
+    // Okを返し終了
+    return Ok(());
+  }
+
+  // タプルベクトルの戻り値を各ベクトルに分解
+  let (fingerprints, scraped_items): (Vec<String>, Vec<ScrapedItem>) = new_data.into_iter().unzip();
+
+  // ----------------------
+  // 更新内容を通知
+  // ----------------------
   // discordに送信
-  discord::send_notify(config.discord.notify_webhook.clone(), items)?;
+  match discord::send_notify(&config.discord.notify_webhook, &scraped_items) {
+    Ok(_) => (),
+    Err(e) => {
+      // 終了処理(未実装)
+      // finish()
 
-  // stateデータ入力テスト
+      // Err返し終了
+      return Err(AppError::Discord(format!("{}", e)));
+    }
+  };
+
+  // ----------------------
+  // state更新
+  // ----------------------
+  // stateに追加
+  state.notified_item_keys.extend(fingerprints);
+  // notified_item_keysが一定以上だったら古いものから消す
+  if state.notified_item_keys.len() > NOTIFIED_KEYS_LIMIT {
+    // オーバー分を計算
+    let overflow = state.notified_item_keys.len() - NOTIFIED_KEYS_LIMIT;
+    // 古い部分を削除
+    state.notified_item_keys.drain(0..overflow);
+  }
+  // state.jsonを更新
   infra::storage::save_state(config.data.dir_path.as_path(), &state)?;
 
-  // detect_historyデータ入力テスト
+  // ----------------------
+  // detect_history更新
+  // ----------------------
   infra::storage::append_detect_history(
     config.data.dir_path.as_path(),
     &shared::DetectHistory {
-      detected_at: chrono::Utc::now().into(),
+      detected_at: output.fetched_at,
       updated: true,
     },
   )?;
 
-  // update_history
-  infra::storage::append_update_history(
-    config.data.dir_path.as_path(),
-    &shared::UpdateHistory {
-      detected_at: chrono::Utc::now().into(),
-      ticker_symbol: "asdgf".to_string(),
-      ticker_name: "asdga".to_string(),
-      published_at: chrono::Utc::now().into(),
-      title: "gfds".to_string(),
-      url: "sdhd".to_string(),
-    },
-  )?;
+  // ----------------------
+  // update_history更新
+  // ----------------------
+  for item in scraped_items {
+    infra::storage::append_update_history(
+      config.data.dir_path.as_path(),
+      &shared::UpdateHistory {
+        detected_at: output.fetched_at,
+        ticker_symbol: item.ticker_symbol,
+        ticker_name: item.ticker_name,
+        published_at: item.published_at,
+        title: item.title,
+        url: item.url,
+      },
+    )?;
+  }
+  // 終了処理(未実装)
+  // finish()
 
   Ok(())
 }
