55c7fac56abb1a1ff989beb41c73e64a6af38087
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-05-27 11:48:01 +0900

    feat: #56 init実行関数を作成する

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,7 +1,9 @@
+use std::fs;
+
 use infra_config::AppConfig;
 use shared::constants::file::NOTIFIED_KEYS_LIMIT;
 use shared::errors::{AppError, AppResult};
-use shared::{ScrapedItem, ScraperOutput, State};
+use shared::{ScrapedItem, ScraperOutput, State, UpdateHistory};
 
 // 外部ライブラリ
 // ログ出力
@@ -10,6 +12,109 @@ use tracing::error;
 /// デバッグ用関数
 pub fn debug() {
   println!("Hello from kernel!");
+}
+
+/// initialize実行関数
+pub fn run_initialize(config: &AppConfig) -> AppResult<()> {
+  // ----------------------
+  // dataディレクトリのリセット
+  // ----------------------
+  // dataが存在したらディレクトリごと削除
+  if path.exists() {
+    match fs::remove_dir_all(path) {
+      Ok(_) => (),
+      Err(e) => return Err(AppError::Process(format!("dataフォルダの削除失敗: {}", e))),
+    };
+  }
+
+  // ディレクトリを再作成（中間ディレクトリも含めて作成）
+  match fs::create_dir_all(path) {
+    Ok(_) => (),
+    Err(e) => return Err(AppError::Process(format!("dataフォルダの作成失敗: {}", e))),
+  };
+
+  // ----------------------
+  // 監視対象サイトをスクレイピング
+  // pythonを実行
+  // ----------------------
+  // スクレイピング部分実行
+  let output: ScraperOutput = match infra::scraper::run_scraper(&config.scraper) {
+    Ok(o) => o,
+    Err(e) => {
+      // 終了処理(未実装)
+      // finish()
+
+      // Err返し終了
+      return Err(AppError::Process(format!("{}", e)));
+    }
+  };
+
+  // ----------------------
+  // 初期stateの作成
+  // ----------------------
+  let mut state = State {
+    creat_state_at: output.fetched_at,
+    last_update_at: output.fetched_at,
+    last_seen_item: ScrapedItem::new(),
+    notified_item_keys: Vec::new(),
+  };
+
+  // ----------------------
+  // 更新差分判定(すべて新規)
+  // ----------------------
+  // Hash生成と取得したItemを認識
+  let new_data: Vec<(String, ScrapedItem)> = monitor::detect_new_item(&output, &state)?;
+
+  // タプルベクトルの戻り値を各ベクトルに分解
+  let (fingerprints, scraped_items): (Vec<String>, Vec<ScrapedItem>) = new_data.into_iter().unzip();
+
+  // ----------------------
+  // state.jsonの作成
+  // ----------------------
+  // 初期stateに追加(一応中身があるかのif文)
+  if let Some(item) = scraped_items.into_iter().next() {
+    state.notified_item_keys.extend(fingerprints);
+    state.last_seen_item = item;
+  }
+  // stateの書き込み
+  infra::storage::save_state(config.data.dir_path.as_path(), &state)?;
+
+  // ----------------------
+  // detect_history作成
+  // ----------------------
+  infra::storage::append_detect_history(
+    config.data.dir_path.as_path(),
+    &shared::DetectHistory {
+      detected_at: output.fetched_at,
+      updated: true,
+    },
+  )?;
+
+  // ----------------------
+  // update_history作成
+  // ----------------------
+  // 一旦初期updatehistoryを作成
+  let mut update_history = UpdateHistory::new();
+  // 時間だけ代入
+  update_history.detected_at = output.fetched_at;
+  // 最新のやつだけupdate_historyに記入
+  if let Some(item) = scraped_items.into_iter().last() {
+    update_history.ticker_symbol = item.ticker_symbol;
+    update_history.ticker_name = item.ticker_name;
+    update_history.published_at = item.published_at;
+    update_history.title = item.title;
+    update_history.url = item.url;
+  }
+  // update_historyに記入
+  infra::storage::append_update_history(config.data.dir_path.as_path(), &update_history)?;
+
+  // initしたことをdiscordに送信(未実装)
+  // discord::send_notify(&config.discord.notify_webhook, &scraped_items)?;
+
+  // 終了処理(未実装)
+  // finish()
+
+  Ok(())
 }
 
 /// monitor実行関数
@@ -30,10 +135,10 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
     Ok(s) => s,
     Err(e) => {
       // デバッグログ
-      error!("Stateの取得に失敗。initフローの実行をしましす:{}", e);
+      error!("Stateの取得に失敗。initフローの実行をします:{}", e);
 
-      // 終了処理(未実装)
-      // finish()
+      // 初回フローの実行
+      run_initialize(config)?;
 
       // Err返し終了
       return Err(AppError::Process(format!(
@@ -51,11 +156,8 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
       // デバッグログ
       //error!("Stateに値なし。initフローの実行をしましす:{}", e);
 
-      // 初回フローの実行(未実装)
-      // init()
-
-      // 終了処理(未実装)
-      // finish()
+      // 初回フローの実行
+      run_initialize(config)?;
 
       // Err返し終了
       return Err(AppError::Process(
@@ -68,7 +170,7 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
   // 監視対象サイトをスクレイピング
   // pythonを実行
   // ----------------------
-  // スクレイピング部分実行のテスト
+  // スクレイピング部分実行
   let output: ScraperOutput = match infra::scraper::run_scraper(&config.scraper) {
     Ok(o) => o,
     Err(e) => {
@@ -112,16 +214,7 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
   // 更新内容を通知
   // ----------------------
   // discordに送信
-  match discord::send_notify(&config.discord.notify_webhook, &scraped_items) {
-    Ok(_) => (),
-    Err(e) => {
-      // 終了処理(未実装)
-      // finish()
-
-      // Err返し終了
-      return Err(AppError::Discord(format!("{}", e)));
-    }
-  };
+  discord::send_notify(&config.discord.notify_webhook, &scraped_items)?;
 
   // ----------------------
   // state更新
@@ -165,6 +258,7 @@ pub fn run_monitor(config: &AppConfig) -> AppResult<()> {
       },
     )?;
   }
+
   // 終了処理(未実装)
   // finish()
 
