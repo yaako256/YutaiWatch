0c76fdb5d465004de5c8a157aa8edf52b4df7605
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-05-27 11:46:03 +0900

    fix: #55 Stateを現在の仕様に沿うように変更する

--- shared/src/models/state.rs
+++ shared/src/models/state.rs
@@ -2,14 +2,16 @@
 shared/src/models/state.rs
 state.jsonfに保存するデータの構造体定義
 */
-
+use super::scraper_output::ScrapedItem;
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct State {
-  pub schema_version: u32,
-  pub last_success_at: String,
-  pub last_snapshot_hash: String,
-  pub last_seen_item_key: String,
+  // pub schema_version: u32,
+  pub creat_state_at: DateTime<FixedOffset>,
+  pub last_update_at: DateTime<FixedOffset>,
+  // pub last_snapshot_hash: String,
+  pub last_seen_item: ScrapedItem,
   pub notified_item_keys: Vec<String>,
 }
