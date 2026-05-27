a5ae49a70318438b82f1d0e1ea76993639426731
yaako256 <281857787+yaako256@users.noreply.github.com>
2026-05-28 01:57:06 +0900

    fix: #83 Dockerがwarkspace用じゃない問題の修正

--- .docker/prod/entrypoint.sh
+++ .docker/prod/entrypoint.sh
@@ -1,6 +1,11 @@
-# docker/prod/entrypoint.sh
 #!/bin/sh
 
-echo "start supercronic"
+# docker/prod/entrypoint.sh
+# If #!/bin/sh is not placed at the very top, it will cause an error!
 
+echo "initialize"
+/app/yutai_watch -- initialize
+
+echo "start supercronic"
 exec /usr/local/bin/supercronic /app/crontab
+
